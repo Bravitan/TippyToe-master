9666e4e33cc4ca8e325f2b0f430555f3bf436e21
bravitan433@gmail.com <bravitan433@gmail.com>
2019-09-10 17:41:37 +0300

    Finally Project

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -53,54 +53,6 @@ namespace TippyToe.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        //Shoes s = new Shoes();
-        //[HttpPost]
-        //public IActionResult Index(CandidateDetail Details, HttpPostedFileBase File1, HttpPostedFileBase File2)
-        //{
-        //    if (File1 != null && File1.ContentLength > 0 && File2 != null)
-        //    {
-        //        Details.BinaryPhoto = new byte[File1.ContentLength]; // file1 to store image in binary formate  
-        //        File1.InputStream.Read(Details.BinaryPhoto, 0, File1.ContentLength);
-        //        string ImageName = System.IO.Path.GetFileName(File2.FileName); //file2 to store path and url  
-        //        string physicalPath = Server.MapPath("~/img/" + ImageName);
-        //        // save image in folder  
-        //        File2.SaveAs(physicalPath);
-        //        // store path in database  
-        //        Details.PathPhoto = "img/" + ImageName;
-        //        s.CandidateDetails.Add(Details);
-        //        s.SaveChanges();
-        //        return PartialView("detail");
-        //    }
-        //    if (File1 != null && File1.ContentLength > 0 && File2 == null)
-        //    {
-        //        Details.BinaryPhoto = new byte[File1.ContentLength]; // file1 to store image in binary formate  
-        //        File1.InputStream.Read(Details.BinaryPhoto, 0, File1.ContentLength);
-        //        s.CandidateDetails.Add(Details);
-        //        s.SaveChanges();
-        //        return PartialView("detail");
-        //    }
-        //    if (File1 == null && File2 != null)
-        //    {
-        //        string ImageName = System.IO.Path.GetFileName(File2.FileName); //file2 to store path and url  
-        //        string physicalPath = Server.MapPath("~/img/" + ImageName);
-        //        // save image in folder  
-        //        File2.SaveAs(physicalPath);
-        //        Details.PathPhoto = "img/" + ImageName;
-        //        s.CandidateDetails.Add(Details);
-        //        s.SaveChanges();
-        //        return PartialView("detail");
-        //    }
-        //    else
-        //    { //if both file1 and file2 are null then we can store others details without any image  
-        //        s.CandidateDetails.Add(Details);
-        //        s.SaveChanges();
-        //        return PartialView("detail");
-        //    }
-        //}
-
-      
-
-
+        
     }
 }
